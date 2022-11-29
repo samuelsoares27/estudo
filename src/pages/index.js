39c1e97a89fa7876178a13f71f41938e6e893221
Samuel Soares Guimarães <samuel.soares@unidesk.com.br>
2022-11-29 16:10:55 -0300

    First project next.js

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <h1>Aprendendo NextJS</h1>
+        <h2>Home</h2>
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/sobre.js
@@ -0,0 +1,8 @@
+
+export default function Sobre() {
+    return (
+        <>
+            <h1>Sobre</h1>
+        </>
+    )
+}
